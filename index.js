938fe15fae06b565fd3fc529fa16f8621d3701c8
BlazeDexX <orchitiadi@gmail.com>
2020-11-07 14:36:03 +0700

    change

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const axios = require("axios")
-const urls = ["https://munari.glitch.me"]
-setInterval(function() {
-            urls.forEach(url => {
-            axios.get(url).catch(() => {});
-        })
-    }, 60 * 1000);
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
 require('./src/index.js')
