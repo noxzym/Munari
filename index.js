f888e89958310016ef92bd2497767a043d839c58
BlazeDexX <orchitiadi@gmail.com>
2020-11-12 20:43:03 +0700

    shoob timer

--- index.js
+++ index.js
@@ -8,4 +8,12 @@
 // const listener = app.listen(process.env.PORT, () => {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
-require('./src/index.js')
+// require('./src/index.js')
+const { ShardingManager } = require('discord.js-light');
+const manager = new ShardingManager('./src/index.js', { 
+	totalShards: 'auto',
+	token: "NzQwMTEyMzUzNDgzNTU0ODU4.XykRVw.EDydgpK7SRPYBC3fPicAmvP1eh4",
+});
+
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.spawn();
