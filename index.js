a5e3906b45f7d647e0905bdf7293d9abd7dfda76
BlazeDexX <orchitiadi@gmail.com>
2020-11-29 22:43:21 +0700

    shard gan

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-require('./src/index.js')
+// require('./src/index.js')
+const { ShardingManager } = require('discord.js')
+const shard = new ShardingManager("./src/index.js", {
+    token: "",
+    autoSpawn: true,
+    respawn: true,
+    totalShards: "auto",
+})
+
+shard.spawn()
+
+shard.on('launch', (shard) => {
+    console.log(`⬇ [SHARD] Shard ID #${shard.id} ⬇`)
+})
