b29fe7e1e001ba27b02fa856b40ee587ab66f59b
BlazeDexX <orchitiadi@gmail.com>
2020-12-04 20:32:45 +0700

    'update'

--- src/commands/admin/ban.js
+++ src/commands/admin/ban.js
@@ -10,85 +10,66 @@ module.exports = {
   ownerOnly: false,
   guildOnly: true,
   async run(bot, message, args) {
-    const prefix = "m!";
-    if (!message.member.hasPermission("BAN_MEMBERS" || "ADMINISTRATOR"))
+    const prefix = 'm!'
+    if (!message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")) return message.reply("You don't have permissions \`BAN_MEMBERS\` or \`ADMINISTRATOR\`");
+    if (!message.guild.me.hasPermission("BAN_MEMBERS" || "ADMINISTRATOR")) return message.channel.send(`Missing permissions for me: \`BAN_MEMBERS\` or \`ADMINISTRATOR\``)
+    let member = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
+    if (!member) return message.channel.send(`usage: ${prefix + this.usage}`)
+    if (!member.kickable)
+      return message.channel.send("This member can't be kicked");
+
+    if (
+      message.guild.me.roles.highest.comparePositionTo(member.roles.highest) < 0
+    ) {
       return message.channel.send(
-        `You don't have permissions \`BAN_MEMBERS\` or \`ADMINISTRATOR\``
+        `My Highest role must be higher than **\`${member.user.username}\`** highest role!`
       );
-    if (!message.guild.me.hasPermission("BAN_MEMBERS" || "ADMINISTRATOR"))
-      return message.channel.send(
-        `Missing permissions for me: \`BAN_MEMBERS\` or \`ADMINISTRATOR\``
-        )
-
-    const member = message.guild.member(message.mentions.members.first()) || message.guild.members.cache.get(args[0]);
-    
-    let wantban;
-    let name;
-
-    if(member.id === undefined) {
-      wantban = client.users.cache.get(args[0]).id
-      name = client.users.cache.get(args[0]).tag
-    } else {
-      if (member.hasPermission("ADMINISTRATOR"))
-        return message.channel.send("This member can't be banned");
-
-      if (
-        message.guild.me.roles.highest.comparePositionTo(member.roles.highest) < 0
-      ) {
-        return message.channel.send(
-          `My Highest role must be higher than **\`${member.user.username}\`** highest role!`
-        );
-      }
-
-      if (member.id === message.author.id)
-        return message.channel.send("You can't banned yourself");
-
-      name = client.users.cache.get(member.id).tag
-      wantban = `${member.id}`
     }
-    if (!wantban) return message.channel.send(`usage: ${prefix + this.usage}`);
 
-    let reason = args.join(" ").slice(22);
-
+    if (member.user.id === message.author.id) {
+      return message.channel.send(`You can't banned yourself`)
+    }
+    if (member.user.id === message.client.user.id) {
+      return message.channel.send(`I can't banned myself`)
+    }
+    let reason = args.slice(1).join(' ');
     if (!reason) {
-      reason = "-";
-    } else {
-      reason = `${reason}`;
+      reason = " - ";
+    }
+    else {
+      reason = `${reason}`
     }
 
     try {
-      var react = await message.channel.send(`Are you sure to Ban **\`${name}\`**?`);
-      await react.react("✅");
-      await react.react("❎");
+      var react = await message.channel.send(`Are you sure to ban **\`${member.user.tag}\`**?`);
+      await react.react('✅');
+      await react.react('❎');
       const filter = (reaction, user) => user.id !== message.client.user.id && user.id === message.author.id;
-      var collector = react.createReactionCollector(filter, {time: 30000});
-      collector.on("collect", (reaction, user) => {
+      var collector = react.createReactionCollector(filter, { time: 20000, errors: ['time'] });
+      collector.on('collect', (reaction, user) => {
         if (collector && !collector.ended) collector.stop();
         switch (reaction.emoji.name) {
           case "✅":
-            reaction.users.remove(user).catch(console.error);
-            react.edit(
-              `<a:yes:765207711423004676> | Banned **\`${name}\`** successful!`
-            );
-            message.guild.members.ban(`${wantban}`, {reason: reason})
-              .catch(e => message.channel.send(`Sorry i couldn't ban this user because ${e}`));
+            reaction.users.remove(user).catch(console.error)
+            react.edit(`<a:yes:765207711423004676> | Banned **\`${member.user.tag}\`** successful!`)
+            member.ban(reason).catch(e => message.channel.send(`Sorry i couldn't kick this user because ${e}`))
             break;
 
           case "❎":
-            reaction.users.remove(user).catch(console.error);
-            react.edit(`<a:no:765207855506522173> | Banned **\`${name}\`** has canceled!`);
+            reaction.users.remove(user).catch(console.error)
+            react.edit(`<a:no:765207855506522173> | Banned **\`${member.user.tag}\`** has canceled!`)
             break;
 
           default:
-            reaction.users.remove(user).catch(console.error);
+            reaction.users.remove(user).catch(console.error)
             break;
         }
-      });
-      collector.on("end", () => {
+      })
+      collector.on('end', () => {
         react.reactions.removeAll().catch(console.error);
-      });
+      })
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
   }
 };
---
+++ src/commands/admin/channelcommands.js
@@ -0,0 +1,94 @@
+const { MessageEmbed, Message } = require('discord.js')
+module.exports = {
+    name: "channel",
+    aliases: ["ch"],
+    category: "Administration",
+    descriptions: "Setting channel",
+    usage: "ch <options>",
+    options: ["--lock", "--unlock"],
+    cooldown: "",
+    ownerOnly: false,
+    guildOnly: true,
+    async run(client, message, args) {
+        if (!message.guild.me.hasPermissions('MANAGE_CHANNELS' || 'ADMINISTRATOR')) return message.channel.send(`I need permissions for **\`MANAGE_CHANNELS\`** or **\`ADMINISTRATOR\`**`)
+        if (!message.member.hasPermissions('MANAGE_CHANNELS' || 'ADMINISTRATOR')) return message.channel.send(`You need permissions for **\`MANAGE_CHANNELS\`** or **\`ADMINISTRATOR\`**`)
+
+        const channel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first() || message.channel
+        const e = new MessageEmbed()
+        .setColor('RED')
+        .setDescription(`\`\`\`md\nUsage: m!channel <channel[mention/id]> <argumen>\nArgumen Options: \n* <--lock>, <--unlock>\nExample: m!channel #general <--lock>\n\`\`\``)
+        if (!channel) return message.channel.send(e)
+
+        if (message.content.includes('--lock')) {
+            try {
+                var react = await message.channel.send(`Are you sure to Lock Channel **\`${channel.name}\`**?`);
+                await react.react('✅');
+                await react.react('❎');
+                const filter = (reaction, user) => user.id !== message.client.user.id && user.id === message.author.id;
+                var collector = react.createReactionCollector(filter, { time: 20000 });
+                collector.on('collect', (reaction, user) => {
+                    if (collector && !collector.ended) collector.stop();
+                    switch (reaction.emoji.name) {
+                        case "✅":
+                            reaction.users.remove(user).catch(console.error)
+                            react.edit(`<a:yes:765207711423004676> | Locked Channel **\`${channel.name}\`** successful!`)
+                            channel.updateOverwrite(message.guild.id, {
+                                SEND_MESSAGES: false,
+                            });
+                            message.channel.send(`Channel ${channel.name} has locked now`)
+                            break;
+
+                        case "❎":
+                            reaction.users.remove(user).catch(console.error)
+                            react.edit(`<a:no:765207855506522173> | Locked Channel **\`${channel}\`** has canceled!`)
+                            break;
+
+                        default:
+                            reaction.users.remove(user).catch(console.error)
+                            break;
+                    }
+                })
+                collector.on('end', () => {
+                    react.reactions.removeAll().catch(console.error);
+                })
+            } catch (e) {
+                console.log(e)
+            }
+        } else if (message.content.includes('--unlock')) {
+            try {
+                var react = await message.channel.send(`Are you sure to Unlock Channel **\`${channel.name}\`**?`);
+                await react.react('✅');
+                await react.react('❎');
+                const filter = (reaction, user) => user.id !== message.client.user.id && user.id === message.author.id;
+                var collector = react.createReactionCollector(filter, { time: 20000, errors: ['time'] });
+                collector.on('collect', (reaction, user) => {
+                    if (collector && !collector.ended) collector.stop();
+                    switch (reaction.emoji.name) {
+                        case "✅":
+                            reaction.users.remove(user).catch(console.error)
+                            react.edit(`<a:yes:765207711423004676> | Unocked Channel **\`${channel.name}\`** successful!`)
+                            channel.updateOverwrite(message.guild.id, {
+                                SEND_MESSAGES: true,
+                            });
+                            message.channel.send(`channel ${channel.name} has unlocked now!`)
+                            break;
+
+                        case "❎":
+                            reaction.users.remove(user).catch(console.error)
+                            react.edit(`<a:no:765207855506522173> | Unlocked Channel **\`${channel}\`** has canceled!`)
+                            break;
+
+                        default:
+                            reaction.users.remove(user).catch(console.error)
+                            break;
+                    }
+                })
+                collector.on('end', () => {
+                    react.reactions.removeAll().catch(console.error);
+                })
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    }
+}
