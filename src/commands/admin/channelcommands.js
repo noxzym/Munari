c7d313f9419f847c71bbc6868848f3814000011a
DexX <67855694+BlazeDexX@users.noreply.github.com>
2020-12-04 22:10:20 +0700

    Update channelcommands.js

--- src/commands/admin/channelcommands.js
+++ src/commands/admin/channelcommands.js
@@ -15,6 +15,13 @@ module.exports = {
 
         const channel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first() || message.channel
 
+        if (channel.id === undefined) {
+            const e = new MessageEmbed()
+                .setColor('RED')
+                .setDescription(`\`\`\`md\nUsage: m!channel <channel[mention/id]> <argumen>\nArgumen Options: \n* <--lock>, <--unlock>\nExample: m!channel #general <--lock>\n\`\`\``)
+            return message.channel.send(e)
+        } 
+
         if (message.content.includes('--lock')) {
             try {
                 var react = await message.channel.send(`Are you sure to Lock Channel **\`${channel.name}\`**?`);
@@ -85,12 +92,6 @@ module.exports = {
             } catch (e) {
                 console.log(e)
             }
-        } else if (!channel) {
-            const e = new MessageEmbed()
-                .setColor('RED')
-                .setDescription(`\`\`\`md\nUsage: m!channel <channel[mention/id]> <argumen>\nArgumen Options: \n* <--lock>, <--unlock>\nExample: m!channel #general <--lock>\n\`\`\``)
-            return message.channel.send(e)
         } 
-        
     }
 }
