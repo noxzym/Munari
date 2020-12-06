7a083078d2eab511ada9322c23e9557caf5505a5
BlazeDexX <orchitiadi@gmail.com>
2020-12-06 10:43:44 +0700

    voice state update

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -2,16 +2,32 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: 'voiceStateUpdate',
     async run(client, oldState, newState) {
-        // if (oldState.channel && oldState.channel.members.size === 1 && oldState.channelID === oldState.guild.voice.channelID) {
-        //     const channel = client.queue.get(message.guild.id).textChannel
-        //     const e = new MessageEmbed()
-        //         .setColor('RED')
-        //         .setDescription(`I will disconnect if no users join my voice channel after 1 menutes`)
-        //     var em = await channel.send(e)
-        //     setTimeout(() => {
-        //         oldState.channel.leave()
-        //         em.delete({timeout: 2000})
-        //     }, 60000)
-        // }
+        if (oldState.channel === null) return newState.setSelfDeaf(true);
+        let change = false;
+        const queue = client.queue.get(newState.guild.id);
+        if (!queue) return;
+        if (newState.member.id === client.user.id && oldState.member.id === client.user.id) {
+            if (newState.member.voice.channel === null) {
+                queue.songs = [];
+                queue.loop = false;
+                return client.queue.delete(newState.guild.id);
+            }
+            if (newState.member.voice.channel !== queue.voiceChannel) {
+                change = true;
+                queue.voiceChannel = newState.member.voice.channel;
+                queue.connection = newState.connection;
+            }
+        }
+        if (oldState.channel.members.size === 1 && oldState.channel === queue.voiceChannel || change) {
+            setTimeout(() => {
+                if (!queue || !queue.connection.dispatcher || queue.connection.dispatcher === null) return;
+                if (queue.voiceChannel.members.size === 1) {
+                    queue.textChannel.send(`I have left the channel as i was left alone.`);
+                    queue.songs = [];
+                    queue.loop = false;
+                    queue.connection.dispatcher.end();
+                }
+            }, 120000);
+        }
     }
 }
