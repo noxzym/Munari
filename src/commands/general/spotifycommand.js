27e7244290f808e7cfebf768427871ae75150572
BlazeDexX <orchitiadi@gmail.com>
2020-12-09 15:32:18 +0700

    update

--- src/commands/general/spotifycommand.js
+++ src/commands/general/spotifycommand.js
@@ -1,5 +1,5 @@
 const { MessageEmbed, MessageAttachment } = require('discord.js')
-// const { Spotify } = require('canvacord')
+const { registerFont, createCanvas, loadImage } = require('canvas')
 module.exports = {
     name: "spotify",
     aliases: [""],
@@ -15,30 +15,45 @@ module.exports = {
 
         const presence = member.presence.activities.filter(x => x.name === 'Spotify')[0]
         if (!presence) return message.channel.send(`I can't find spotify presence, try again`)
-
-        const songname = presence.details
-        const album = presence.assets.largeText
-        const canvatitle = presence.details
+        
+        const songname = presence.details.length > 15 ? presence.details.replace(presence.details.substr(15, 1000), " ...") : presence.details
+        const album = presence.assets.largeText.length > 18 ? presence.assets.largeText.replace(presence.assets.largeText.substr(18, 1000), " ...") : presence.assets.largeText
+        const auth = presence.state.length > 20 ? presence.state.replace(presence.state.substr(20, 1000), ' ...') : presence.state
         const title = `${presence.state} • ${presence.details}`
         const url = `https://open.spotify.com/track/${presence.syncID}`
         const img = `https://i.scdn.co/image/${presence.assets.largeImage.slice(8)}`
-        const auth = presence.state
 
         const start = presence.timestamps.start
         const end = presence.timestamps.end
 
-        // if (message.content.includes('--card')) {
-        //     const spotify = new Spotify()
-        //         .setAuthor(auth)
-        //         .setTitle(canvatitle)
-        //         .setImage(img)
-        //         .setAlbum(album)
-        //         .setStartTimestamp(start)
-        //         .setEndTimestamp(end)
-        //     const buffer = await spotify.build()
-        //     const ath = new MessageAttachment(buffer, 'Spotify.png')
-        //     message.channel.send(member, ath)
-        // } else {
+        if (message.content.includes('--card')) {
+            registerFont('notoserifsemi.otf', { family: 'Noto Serif JP' })
+
+            const canvas = createCanvas(900, 285)
+            const ctx = canvas.getContext('2d')
+            const bg = await loadImage('https://cdn.discordapp.com/attachments/767235205202444309/786139633192927242/images.png')
+            ctx.drawImage(bg, 0, 0, canvas.width, canvas.height)
+
+            const img2 = await loadImage(img)
+            ctx.drawImage(img2, 650, 30, 220, 220)
+
+            ctx.font = "bold 30px Noto Serif JP"
+            ctx.fillStyle = '#FFFFFF'
+            ctx.fillText('Spotify • ' + album, 35, 50)
+
+            ctx.font = "bold 50px Noto Serif JP"
+            ctx.fillStyle = '#FFFFFF'
+            ctx.fillText(songname, 40, canvas.height / 2)
+
+            ctx.font = "bold 30px Noto Serif JP"
+            ctx.fillStyle = '#FFFFFF'
+            ctx.fillText(auth, 40, canvas.height / 2 + 50)
+
+            const image = canvas.toBuffer()
+            const ath = new MessageAttachment(image, "spotify,png")
+
+            message.channel.send(member, ath)
+        } else {
             let e = new MessageEmbed()
                 .setColor('18d869 ')
                 .setAuthor(`Spotify Song Information`, 'https://media.discordapp.net/attachments/570740974725103636/582005158632882176/Spotify.png')
@@ -49,6 +64,6 @@ module.exports = {
                 .setTimestamp()
                 .setFooter(`Commanded by ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
             message.channel.send(e)
-        // }
+        }
     }
 }
