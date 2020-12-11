c2616944ce60c4e56c83a1d43f65e5fcecba5e9b
BlazeDexX <orchitiadi@gmail.com>
2020-12-11 19:59:14 +0700

    allright typo

--- src/utils/command.js
+++ src/utils/command.js
@@ -8,15 +8,17 @@ module.exports = (client) => {
         const commands = readdirSync(`./src/commands/${dir}/`).filter(file => file.endsWith(".js"));
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
-
+    
             if (pull.name) {
                 client.commands.set(pull.name, pull);
                 // table.addRow(file, `✔️  -> command berhasil di load`);
             } else {
                 console.log(file, `❎  -> command.name atau help.name tidak dapat ditemukan`);
                 continue;
-            }
+            } 
+    
+            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
         }
-
+  
     });
 }
