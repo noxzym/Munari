25376a6d9a0f1a515ab14b0d5cdc56a962657597
BlazeDexX <orchitiadi@gmail.com>
2020-12-11 19:42:34 +0700

    enable sharding

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-require('./src/index.js')
-//  const { ShardingManager } = require('discord.js');
-//  const mainFile = "./src/index.js";
-//  const shards = new ShardingManager(`${mainFile}`, {
-//      token: 'NzQwMTEyMzUzNDgzNTU0ODU4.XykRVw.EDydgpK7SRPYBC3fPicAmvP1eh4',
-//  });
-//  shards.on('shardCreate', shard => {
-//      console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${shard.id}`);
-//  });
-//  shards.on('message', (shard, msg) => {
-//      console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${msg._eval} | ${msg._result}`);
-//  });
-//  shards.spawn(2, undefined, -1)
+// require('./src/index.js')
+const { ShardingManager } = require('discord.js');
+const mainFile = "./src/index.js";
+
+process.on("unhandledRejection", e => {
+    console.error(`Error handler caught an error: \n${e.stack}`);
+});
+
+process.on("uncaughtException", e => {
+    console.error(`Error handler caught an error: \n${e.stack}`);
+    console.info("Fatal error has been detected. Exiting processing...");
+
+    process.exit(1);
+});
+
+const shards = new ShardingManager(`${mainFile}`, {
+    totalShards: 'auto',
+    mode: 'process',
+    respawn: true,
+    token: 'NzQwMTEyMzUzNDgzNTU0ODU4.XykRVw.EDydgpK7SRPYBC3fPicAmvP1eh4'
+});
+
+shards.on('shardCreate', shard => {
+    console.log(`[ShardManager] Shard #${shard.id} has spawned`);
+    shard.on("disconnect", () => {
+        console.log(`[ShardManager] Shard #${shard.id} has disconnected`);
+    });
+    shard.on("reconnecting", () => {
+        console.log(`[ShardManager] Shard #${shard.id} has reconnected.`);
+    });
+    if (shards.shards.size === shards.totalShards) console.log("[ShardManager] All shards spawned successfully.");
+});
+shards.on('message', (shard, msg) => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${msg._eval} | ${msg._result}`);
+});
+shards.spawn(shards.totalShards, undefined, -1).catch(e => console.log("SHARD_SPAWN_ERR: ", e));
--- src/utils/command.js
+++ src/utils/command.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js')
 const { readdirSync } = require("fs");
-const ascii = require("ascii-table");
-let table = new ascii("Commands");
-table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
 
@@ -11,18 +8,15 @@ module.exports = (client) => {
         const commands = readdirSync(`./src/commands/${dir}/`).filter(file => file.endsWith(".js"));
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
-    
+
             if (pull.name) {
                 client.commands.set(pull.name, pull);
                 // table.addRow(file, `✔️  -> command berhasil di load`);
             } else {
-                table.addRow(file, `❎  -> command.name atau help.name tidak dapat ditemukan`);
+                console.log(file, `❎  -> command.name atau help.name tidak dapat ditemukan`);
                 continue;
-            } 
-    
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+            }
         }
-  
+
     });
-    console.log(table.toString());
 }
