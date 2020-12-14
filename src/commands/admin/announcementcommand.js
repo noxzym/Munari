a2c192ec905c49018cab0474a5db5f018e3e650c
BlazeDexX <orchitiadi@gmail.com>
2020-12-14 21:18:43 +0700

    kontol

--- src/commands/admin/announcementcommand.js
+++ src/commands/admin/announcementcommand.js
@@ -10,24 +10,21 @@ module.exports = {
   ownerOnly: false,
   guildOnly: true,
   async run(client, message, args) {
-  const prefix = 'm!'
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`You don't have permissions \`ADMINISTRATOR\``);
+    const prefix = 'm!'
+    if (!message.member.hasPermission("ADMINISTRATOR" || "MANAGE_MESSAGES")) return message.channel.send(`You don't have permissions \`ADMINISTRATOR\``);
     let rest = args.slice(1).join(' ')
-    if(!rest) return message.channel.send(`Usage: ${prefix + this.usage}`)
+    if (!rest) return message.channel.send(`Usage: ${prefix + this.usage}`)
     let array_of_arguments = rest.split(',.')
     let embed = new Discord.MessageEmbed()
       .setTitle(array_of_arguments[0])
       .setDescription(array_of_arguments[1])
-      .setColor(
-        message.member.roles.cache
-          .sort((a, b) => b.position - a.position)
-          .first().color)
+      .setColor(message.member.displayHexColor)
       .setImage(array_of_arguments[2] || null)
-  //    .setThumbnail(message.guild.iconURL({  dynamic: true, size : 512  }))
-      .setFooter(`Announce Message by: ${message.author.tag}`, message.author.avatarURL({dynamic: true}))
+      .setThumbnail(message.guild.iconURL({ dynamic: true, size: 512 }))
+      .setFooter(`Announce Message by: ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
       .setTimestamp();
     let channel = message.mentions.channels.first()
-    if(!channel) return message.channel.send(`Please input channel first`)
+    if (!channel) return message.channel.send(`Please input channel first`)
     channel.send({ embed });
   }
 }
