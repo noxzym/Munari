2fac56e60165ccd3e5313ac7e83764ba5004472a
BlazeDexX <orchitiadi@gmail.com>
2020-12-20 11:29:29 +0700

    fix error

--- src/commands/utility/votecommand.js
+++ src/commands/utility/votecommand.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, MessageAttachment } = require('discord.js')
 const { get } = require('axios')
 module.exports = {
     name: "vote",
@@ -11,7 +11,7 @@ module.exports = {
     ownerOnly: false,
     guildOnly: true,
     async run(client, message, args) {
-        let status = 'https://top.gg/api/widget/740112353483554858.svg'.replace('svg', 'png')
+        let ath = new MessageAttachment('https://top.gg/api/widget/740112353483554858.png', 'topgg.png')
         let topgg = await client.dbl.getBot(client.user.id).then(x => x.monthlyPoints)
         let dblist = await get('https://discordbotlist.com/api/v1/bots/740112353483554858').then(x => x.data.upvotes)
         let e = new MessageEmbed()
@@ -19,9 +19,9 @@ module.exports = {
             .setAuthor(`Vote me in this website`, client.user.avatarURL({ size: 4096 }))
             .setDescription(`**[[\`top.gg\`](https://top.gg/bot/740112353483554858/vote)] • \`${topgg}\` Votes\n[[\`discord.boats\`](https://discord.boats/bot/740112353483554858/vote)] • \`-\` Votes\n[[\`discordbotlist.com\`](https://discordbotlist.com/bots/munari-rose/upvote)] • \`${dblist}\` Votes**`)
             .setThumbnail(client.user.avatarURL({ size: 4096 }))
-            .setImage(status)
+            .setImage('attachment://topgg.png')
             .setTimestamp()
             .setFooter(`Commanded by ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
-        message.channel.send(e)
+        message.channel.send({files: [ath], embed: e})
     }
 }   
