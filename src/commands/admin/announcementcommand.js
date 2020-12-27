986147d11358e5868c1648250a7ebcc7e3db2cf7
BlazeDexX <orchitiadi@gmail.com>
2020-12-27 12:24:05 +0700

    update announcement command

--- src/commands/admin/announcementcommand.js
+++ src/commands/admin/announcementcommand.js
@@ -4,27 +4,49 @@ module.exports = {
   aliases: ["anc"],
   category: "Administration",
   descriptions: "Send Announcement message to specific channel",
-  usage: "announcement <channel> [Title]<,.>[Description]<,.>[Image]",
+  usage: "announcement <#channel> <json embed>",
   options: [""],
   cooldown: "",
   ownerOnly: false,
   guildOnly: true,
   async run(client, message, args) {
-    const prefix = 'm!'
     if (!message.member.hasPermission("ADMINISTRATOR" || "MANAGE_MESSAGES")) return message.channel.send(`You don't have permissions \`ADMINISTRATOR\``);
-    let rest = args.slice(1).join(' ')
-    if (!rest) return message.channel.send(`Usage: ${prefix + this.usage}`)
-    let array_of_arguments = rest.split(',.')
-    let embed = new Discord.MessageEmbed()
-      .setTitle(array_of_arguments[0])
-      .setDescription(array_of_arguments[1])
-      .setColor(message.member.displayHexColor)
-      .setImage(array_of_arguments[2] || null)
-      .setThumbnail(message.guild.iconURL({ dynamic: true, size: 512 }))
-      .setFooter(`Announce Message by: ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
-      .setTimestamp();
-    let channel = message.mentions.channels.first()
+
+    let data;
+    try {
+      data = JSON.parse(args.slice(1).join(' ').toString())
+    } catch (e) {
+      return message.channel.send({ embed: { color: 'ff0000', description: `Please input like **[\`this\`](https://hastebin.com/ipupotitis.rb)**` } }).then(x => { x.delete({ timeout: 5000 }) })
+    }
+
+    let color = data.color !== undefined ? data.color : '0099ff';
+    let authorname = data.author && data.author.name !== undefined ? data.author.name : message.guild.name;
+    let authorurl = data.author && data.author.icon_url !== undefined ? data.author.icon_url : message.guild.iconURL({ size: 4096, dynamic: true });
+    let title = data.title !== undefined ? data.title : "Announcement";
+    let url = data.url !== undefined ? data.url : null;
+    let description = data.description !== undefined ? data.description : null;
+    let footertext = data.footer && data.footer.text !== undefined ? data.footer.text : message.author.name;
+    let footericon = data.footer && data.footer.icon_url !== undefined ? data.footer.icon_url : message.author.avatarURL({ dynamic: true });
+
+    let channel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first()
     if (!channel) return message.channel.send(`Please input channel first`)
-    channel.send({ embed });
+    channel.send({
+      embed:
+      {
+        color: color,
+        author: {
+          name: authorname,
+          icon_url: authorurl
+        },
+        title: title,
+        url: url,
+        description: description,
+        timestamp: new Date(),
+        footer: {
+          icon_url: footericon,
+          text: footertext
+        }
+      }
+    });
   }
 }
