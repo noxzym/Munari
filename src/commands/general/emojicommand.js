c3bf9c6a20ef415d43cbb691429e17eb916513bb
BlazeDexX <orchitiadi@gmail.com>
2021-01-08 12:01:05 +0700

    fix emoji command

--- src/commands/general/emojicommand.js
+++ src/commands/general/emojicommand.js
@@ -24,13 +24,11 @@ module.exports = {
         const image = eminfo.animated === false ? `https://cdn.discordapp.com/emojis/${eminfo.id}.png?size=4096` : `https://cdn.discordapp.com/emojis/${eminfo.id}.gif`
         const name = eminfo.name
         const ID = eminfo.id
-        const createdAt = moment(client.emojis.cache.get(ID).createdAt).format('MMMM Do YYYY, h:mm:ss a');
-        if (!createdAt) return message.channel.send(`I can't get this emoji because i am not join server this emoji`)
-        const animated = eminfo.animated ? 'True' : 'False'
+        const createdAt = client.emojis.cache.get(ID) === undefined ? "Unknown" : moment(client.emojis.cache.get(ID).createdAt).format('MMMM Do YYYY, h:mm:ss a');
+        const animated = eminfo.animated ? 'Yes' : 'No'
         const information = eminfo.animated === false ? `<:${eminfo.name}:${eminfo.id}>` : `<a:${eminfo.name}:${eminfo.id}>`
         let e = new MessageEmbed()
         .setColor(message.member.displayHexColor)
-        .setAuthor(`Emoji Command`)
         .setDescription(`**Name: \`${name}\`\nID: \`${ID}\`\nAnimated: \`${animated}\`\nIdentifier: \`${information}\`\nCreatedAt: \`${createdAt}\`**`)
         .setImage(image)
         message.channel.send(e)
