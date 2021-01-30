b32dc73ed05c4de5863028786465f8ea2a811b25
DexX <67855694+BlazeDexX@users.noreply.github.com>
2021-01-30 17:55:59 +0700

    Fix: AvatarCommand

--- src/commands/general/avatarcommand.js
+++ src/commands/general/avatarcommand.js
@@ -11,10 +11,8 @@ module.exports = {
   guildOnly: true,
   async run(client, message, args) {
     if (args.slice(0).join("").match(/^(?:--server)$/g)) {
-      let e = createEmbed()
-        .setTitle(`${member.user.tag}`)
-        .setURL(`${member.user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
-        .setColor(color)
+      let e = createEmbed("info")
+        .setAuthor(`${message.guild.name}`, message.guild.iconURL({ dynamic: true, size: 4096, format: "png" }), message.guild.iconURL({ dynamic: true, size: 4096, format: "png" }))
         .setImage(message.guild.iconURL({ format: 'png', dynamic: true, size: 4096 }))
         .setFooter(`Commanded by ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
         .setTimestamp();
@@ -26,24 +24,12 @@ module.exports = {
       message.guild.members.cache.find(x => x.user.username.toLowerCase() === `${args[0]}` || x.user.username === `${args[0]}`) ||
       message.mentions.members.first() ||
       message.member;
-
-    let memava = member.user.avatarURL({ format: "png", dynamic: true })
-    let color = message.member.displayHexColor
-    let e = createEmbed()
-      .setTitle(`${member.user.tag}`)
-      .setURL(`${member.user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
-      .setColor(color)
+    
+    let e = createEmbed("info")
+      .setAuthor(`${member.user.tag}`, member.user.avatarURL({ format: "png", dynamic: true, size: 4096 }), member.user.avatarURL({ format: "png", dynamic: true, size: 4096 }))
+      .setImage(member.user.avatarURL({ format: "png", dynamic: true, size: 4096 }))
       .setFooter(`Commanded by ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
       .setTimestamp();
-
-    if (memava.split(".").pop() === "gif") {
-      e.setImage(`${memava}?size=4096`);
-
-      return message.channel.send(e)
-    } else {
-      e.setImage(`${memava}?size=4096`)
-
-      return message.channel.send(e)
-    }
+    message.channel.send(e)
   }
 };
